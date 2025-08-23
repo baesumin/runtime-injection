d15a5c66bc1dc9a995ddf11279575375808a7b8d
baesumin <qotnals12345@naver.com>
2025-08-23 15:01:07 +0900

    feat: modal,table 컴포넌트 제작

--- apps/app2/src/components/ShoppingList.tsx
+++ apps/app2/src/components/ShoppingList.tsx
@@ -1,6 +1,66 @@
-import { useItems } from "../store/store";
+import { useState } from "react";
+import { Item, useItems } from "../store/store";
+import Button from "./Button";
+import Table from "./Table";
+import Modal from "./Modal";
 
 export default function ShoppingList() {
-  const { items } = useItems();
-  return <></>;
+  const { items, deleteItem } = useItems();
+  const [selectedItem, setSelectedItem] = useState<Item | undefined>();
+
+  return (
+    <>
+      {selectedItem !== undefined && (
+        <Modal
+          open={selectedItem !== undefined}
+          title={"상품 상세"}
+          onClose={() => setSelectedItem(undefined)}
+        >
+          asdfsdaf
+        </Modal>
+      )}
+      <Table
+        columns={[
+          {
+            key: "name",
+            title: "물품명",
+          },
+          {
+            key: "price",
+            title: "가격",
+            render(value: number) {
+              return value.toLocaleString() + "원";
+            },
+          },
+          {
+            key: "count",
+            title: "수량",
+            render(value: number) {
+              return value.toLocaleString() + "개";
+            },
+          },
+          {
+            key: "id",
+            title: "처리",
+            render(value: string) {
+              return (
+                <Button
+                  buttonType="cancel"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    deleteItem(value);
+                  }}
+                >
+                  삭제
+                </Button>
+              );
+            },
+          },
+        ]}
+        datas={items}
+        rowKey={(data) => data.id}
+        onRowClick={(data) => setSelectedItem(data)}
+      />
+    </>
+  );
 }
